129ae01011e56e9bc9249550d074809feafcfe84
Qziem <kamilkuz@gmail.com>
2018-09-07 00:22:15 +0200

    Ustawinie utf-8 przy polaczeniu do bazy

--- be/services/svc.php
+++ be/services/svc.php
@@ -13,7 +13,12 @@ $app = new \Slim\App(['settings' => $config]);
 $container = $app->getContainer();
 $container['db'] = function ($container) {
     $db = $container["settings"]["db"];
-    $pdo = new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'], $db['user'], $db['pass']);
+    $pdo = new PDO(
+        'mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],
+        $db['user'],
+        $db['pass'],
+        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+    );
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $pdo->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);
