83bcbe22834ed088f2862fafca4f3d62e294df14
Qziem <kamilkuz@gmail.com>
2018-09-09 21:26:02 +0200

    Wywalenie konfiguracji pdo

--- be/container.php
+++ be/container.php
@@ -7,21 +7,6 @@ use Doctrine\ORM\Tools\Setup;
 
 $container = new Slim\Container(['settings' => $config]);
 
-$container['db'] = function ($container) {
-    $db = $container["settings"]["db"];
-    $pdo = new PDO(
-        'mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],
-        $db['user'],
-        $db['pass'],
-        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
-    );
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    $pdo->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);
-    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    return $pdo;
-};
-
 $container['em'] = function ($container) {
     $config = \Doctrine\ORM\Tools\Setup::createAnnotationMetadataConfiguration(
         $container['settings']['doctrine']['metadata_dirs'],
