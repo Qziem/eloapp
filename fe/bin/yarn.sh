802c7279ccfac91df8b01d580fedadd8b03815e7
Qziem <kamilkuz@gmail.com>
2019-01-30 19:25:13 +0100

    Docker dla fe według Adamowych wskazówek

--- fe/bin/yarn.sh
+++ fe/bin/yarn.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cd `dirname $0`/../../
-docker-compose exec elo_fe yarn $*
+ENV_PATH=`pwd`"/"`dirname $0`/../../
+source $ENV_PATH.env
+FE_PATH=$SOURCE_DIR/fe
+cd $FE_PATH
+
+docker run --rm -it -w /home/node/app -v $FE_PATH:/home/node/app -u node node:10 yarn $*
 
 exit $?
